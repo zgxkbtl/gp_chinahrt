9827116e70a72ffc1ff1ee6a58794c91b68d0356
zgxkbtl <489693132@qq.com>
2024-10-17 00:21:37 +0800

    license

--- src/js/chinahrt.js
+++ src/js/chinahrt.js
@@ -1,13 +1,14 @@
 // ==UserScript==
-// @name         chinahrt
+// @name         gp_chinahrt
 // @namespace    http://tampermonkey.net/
 // @version      2024-10-15
-// @description  try to take over the world!
+// @description  auto play video for gp.chinahrt.com
 // @author       You
 // @match        https://*.chinahrt.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=chinahrt.com
 // @run-at       document-start
 // @grant        none
+// @license      MIT
 // ==/UserScript==
 
 (function() {
